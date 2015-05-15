e5a3661d3a41abdfb32190fd867034f25ec29978
deepigaa <deepigaa1gayathri@gmail.com>
2015-05-15 08:47:08 -0400

    first commit

---
+++ src/test/java/com/learntestng/AppTest.java
@@ -0,0 +1,10 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+  @Test
+  public void sendMail() {
+	  System.out.println("sending mail");
+  }
+}
---
+++ src/test/java/com/learntestng/SampleTest.java
@@ -0,0 +1,10 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+  @Test
+  public void f() {
+	  System.out.println("Hello world");
+  }
+}
